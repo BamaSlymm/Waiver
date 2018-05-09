0b578c46ca65c1abbda487831b54ab7e2e16f4ea
Dogan Cibiceli - OIT <dogan.cibiceli@state.co.us>
2018-05-09 14:05:10 -0600

    Closes issue #16. Service - Design Waiver Form

--- Pages/CreateWaiverStep1.cs
+++ Pages/CreateWaiverStep1.cs
@@ -1,17 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DPAWaiver.Pages
 {
     public class CreateWaiverStep1Model : PageModel
     {
-        public void OnGet()
+        public List<SelectListItem> types { set; get; }
+
+        [BindProperty]
+        [Range(1, int.MaxValue)]
+        public int selectedType { set; get; }
+        public async Task OnGetAsync()
         {
+            types = new List<SelectListItem> {
+                new SelectListItem {Text = "Data Entry", Value = "1"},
+                new SelectListItem {Text = "Design Services", Value = "2"},
+                new SelectListItem {Text = "Mail Service", Value = "3"},
+                new SelectListItem {Text = "Print / Copy", Value = "4"},
+                new SelectListItem {Text = "Scanning / Imaging / Microfilm", Value = "5"},
+            };
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                types = new List<SelectListItem> {
+                new SelectListItem {Text = "Data Entry", Value = "1"},
+                new SelectListItem {Text = "Design Services", Value = "2"},
+                new SelectListItem {Text = "Mail Service", Value = "3"},
+                new SelectListItem {Text = "Print / Copy", Value = "4"},
+                new SelectListItem {Text = "Scanning / Imaging / Microfilm", Value = "5"},
+                };
+                return Page();
+            }
+            if (selectedType == 1)
+            {
+                return RedirectToPage("./CreateWaiverServiceDataEntry");
+            }
+            return RedirectToPage("./CreateWaiverServiceDesign");
 
         }
+
+
     }
 }
