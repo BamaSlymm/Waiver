864e563c9f9c8af6861140377e4ac376f94dc603
Dogan Cibiceli - OIT <dogan.cibiceli@state.co.us>
2018-08-15 13:02:21 -0600

    Closes #50. Convert user sign in to a partial view

--- DPAWaiver/Pages/CreateWaiverServiceDataEntry.cs
+++ DPAWaiver/Pages/CreateWaiverServiceDataEntry.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DPAWaiver.Areas.Identity.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace DPAWaiver.Pages
 {
     public class CreateWaiverServiceDataEntryModel : PageModel
     {
-        public void OnGet()
-        {
 
+        private Task<DPAUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+        public DPAUser signedInUser {get;set;}
+
+       private readonly UserManager<DPAUser> _userManager;
+
+        public CreateWaiverServiceDataEntryModel(UserManager<DPAUser> userManager)
+        {
+            _userManager = userManager;
+            
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            signedInUser = _userManager.Users.Include(x=>x.Department).Single(x=>x.Id == user.Id);
+            return Page();
         }
     }
 }
